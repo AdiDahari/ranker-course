860958752f6bf255d7644cd8b796bf7893a0848e
Adi Dahari <dahariadi16@gmail.com>
2022-09-30 17:23:08 +0300

    progress flow and id generation added

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,23 +1,33 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { CreatePollDto, JoinPollDto } from './dtos';
+import { PollsService } from './polls.service';
 
 @Controller('polls')
 export class PollsController {
+  constructor(private pollsService: PollsService) {}
   @Post()
   async create(@Body() createPollDto: CreatePollDto) {
-    Logger.log('In Create Method');
-    return createPollDto;
+    const result = await this.pollsService.createPoll(createPollDto);
+
+    return result;
   }
 
   @Post('/join')
   async join(@Body() joinPollDto: JoinPollDto) {
-    Logger.log('In Join Method');
-    return joinPollDto;
+    const result = await this.pollsService.joinPoll(joinPollDto);
+
+    return result;
   }
 
   @Post('/rejoin')
   async rejoin() {
-    Logger.log('In Rejoin Method');
+    const result = await this.pollsService.rejoinPoll({
+      name: 'From Token',
+      pollID: 'Also from Token',
+      userID: 'Also from Token #2',
+    });
+
+    return result;
   }
 }
