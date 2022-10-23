408283b7d0c0c86594f276d8737b5ed4d1752352
Adi Dahari <dahariadi16@gmail.com>
2022-10-23 13:59:28 +0300

    pre-poll ui

--- client/src/pages/WaitingRoom.tsx
+++ client/src/pages/WaitingRoom.tsx
@@ -1,7 +1,39 @@
-import React, { useEffect } from 'react';
-import { actions } from '../state';
+import React, { useEffect, useState } from 'react';
+import { BsPencilSquare } from 'react-icons/bs';
+import { MdContentCopy, MdPeopleOutline } from 'react-icons/md';
+import { useCopyToClipboard } from 'react-use';
+import { useSnapshot } from 'valtio';
+import NominationForm from '../components/NominationForm';
+import ParticipantList from '../components/ParticipantList';
+import ConfirmationDialog from '../components/ui/ConfirmationDialog';
+
+import { actions, state } from '../state';
+import { colorizeText } from '../util';
 
 export const WaitingRoom: React.FC = () => {
+  const [_copiedText, copyToClipboard] = useCopyToClipboard();
+  const [isParticipantListOpen, setIsParticipantListOpen] = useState(false);
+  const [isNominationFormOpen, setIsNominationFormOpen] = useState(false);
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+  const [participantToRemove, setParticipantToRemove] = useState<string>();
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const currentState = useSnapshot(state);
+
+  const confirmRemoveParticipant = (id: string) => {
+    setConfirmationMessage(
+      `Remove ${currentState.poll?.participants[id]} from poll?`
+    );
+    setParticipantToRemove(id);
+    setIsConfirmationOpen(true);
+  };
+
+  const submitRemoveParticipant = () => {
+    participantToRemove && actions.removeParticipant(participantToRemove);
+    setIsConfirmationOpen(false);
+  };
+
   useEffect(() => {
     console.log('Waiting Room useEffect');
 
@@ -9,8 +41,105 @@ export const WaitingRoom: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex flex-col w-full justify-between items-center h-full">
-      <h3>Waiting Room</h3>
-    </div>
+    <>
+      <div className="flex flex-col w-full justify-between items-center h-full">
+        <div>
+          <h2 className="text-center">Poll Topic</h2>
+          <p className="italic text-center mb-4">{currentState.poll?.topic}</p>
+          <h2 className="text-center">Poll ID</h2>
+          <h3 className="text-center mb-2">Click to Copy!</h3>
+          <div
+            onClick={() => copyToClipboard(currentState.poll?.id || '')}
+            className="mb-4 flex justify-center align-middle cursor-pointer"
+          >
+            <div className="font-extrabold text-center mr-2">
+              {currentState.poll && colorizeText(currentState.poll?.id)}
+            </div>
+            <MdContentCopy size={24} />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <button
+            onClick={() => setIsParticipantListOpen(true)}
+            className="box btn-orange mx-2 pulsate"
+          >
+            <MdPeopleOutline size={24} />
+            <span>{currentState.participantCount}</span>
+          </button>
+          <button
+            onClick={() => setIsNominationFormOpen(true)}
+            className="box btn-purple mx-2 pulsate"
+          >
+            <BsPencilSquare size={24} />
+            <span>{currentState.nominationCount}</span>
+          </button>
+        </div>
+        <div className="flex flex-col justify-center">
+          {currentState.isAdmin ? (
+            <>
+              <div className="my-2 italic">
+                {currentState.poll?.votesPerVoter} Nominations Required to
+                Start!
+              </div>
+              <button
+                disabled={!currentState.canStartVote}
+                onClick={() => console.log('will add start vote next time!')}
+                className="box btn-orange my-2"
+              >
+                Start Voting
+              </button>
+            </>
+          ) : (
+            <div className="my-2 italic">
+              Waiting for Admin,{' '}
+              <span className="font-extrabold">
+                {currentState.poll?.participants[currentState.poll?.adminID]}
+              </span>
+              , to start the voting.
+            </div>
+          )}
+          <button
+            onClick={() => setShowConfirmation(true)}
+            className="box btn-purple my-2"
+          >
+            Leave Poll
+          </button>
+          <ConfirmationDialog
+            message="Are you sure about leaving the poll>"
+            showDialog={showConfirmation}
+            onCancel={() => setShowConfirmation(false)}
+            onConfirm={() => actions.startOver()}
+          />
+        </div>
+      </div>
+      <ParticipantList
+        isOpen={isParticipantListOpen}
+        onClose={() => setIsParticipantListOpen(false)}
+        participants={currentState.poll?.participants}
+        onRemoveParticipant={confirmRemoveParticipant}
+        isAdmin={currentState.isAdmin || false}
+        userID={currentState.me?.id}
+      />
+      <NominationForm
+        title={currentState.poll?.topic}
+        isOpen={isNominationFormOpen}
+        onClose={() => setIsNominationFormOpen(false)}
+        onSubmitNomination={(nominationText) =>
+          actions.nominate(nominationText)
+        }
+        nominations={currentState.poll?.nominations}
+        userID={currentState.me?.id}
+        onRemoveNomination={(nominationID) =>
+          actions.removeNomination(nominationID)
+        }
+        isAdmin={currentState.isAdmin || false}
+      />
+      <ConfirmationDialog
+        showDialog={isConfirmationOpen}
+        message={confirmationMessage}
+        onConfirm={() => submitRemoveParticipant()}
+        onCancel={() => setIsConfirmationOpen(false)}
+      />
+    </>
   );
 };
